422b54a7020a1634e3f0a01a963f63d51c85ac46
rinalim <ooll00@naver.com>
2023-05-29 20:40:57 +0900

    Update game.py

--- game.py
+++ game.py
@@ -15,6 +15,7 @@ case_lose = {"sci":"rock", "rock":"paper", "paper":"sci"}
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
 pygame.init()
+pygame.mouse.set_visible(False) 
 
 # Set up the drawing window
 screen = pygame.display.set_mode(

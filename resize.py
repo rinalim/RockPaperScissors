480fe35738b235d63f321fa85b91630a80130b15
rinalim <ooll00@naver.com>
2023-06-21 21:06:42 +0900

    Update resize.py

--- resize.py
+++ resize.py
@@ -2,20 +2,18 @@ import os,sys
 import glob
 from PIL import Image
 
-if not os.path.exists('./img_org'):
-    os.system('cp -r ./img ./img_org')
+source = sys.argv[1]
+width = sys.argv[2]
+height = sys.argv[3]
 
-width = sys.argv[1]
-height = sys.argv[2]
-
-files = glob.glob('./img_org/*.png')
+files = glob.glob('./' + source + '/*.png')
 
 for f in files:
     img = Image.open(f)
     img_resize = img.resize((int(width), int(height)))
-    if len(sys.argv) == 4 and sys.argv[3] == 'rotate':
+    if len(sys.argv) == 5 and sys.argv[4] == 'rotate':
         img_resize = img_resize.rotate(180)
     title, ext = os.path.splitext(f)
-    title = title.replace('img_org', 'img')
+    title = title.replace(source, 'img')
     img_resize.save(title + ext)
     print("resizing " + title + ext)
